6cd8cc3ca57a8ffb8a3c4270f2e68e0fec821767
Swara <patilswara4410@gmail.com>
2025-07-22 19:29:53 +0530

    Add STATIC_ROOT for Render deployment

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Optional, if you're using a /static directory for custom files
+STATIC_ROOT = BASE_DIR / "staticfiles"    # Required for collectstatic (especially on Render)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
